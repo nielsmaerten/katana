71072ca28f077a7f30907e7ae1a9d58f6f9c2a1b
Niels Maerten <niels@maerten.be>
2025-06-07 10:07:34 +0200

    remove SSH security settings configuration from late_command script

--- 100-bare-metal-debian/late_command.sh
+++ 100-bare-metal-debian/late_command.sh
@@ -22,8 +22,3 @@ echo "127.0.1.1 katana.local katana" >> /etc/hosts
 # Configure login banner with IP address
 IP=$(ip route get 1.1.1.1 | grep -oP "src \\K\\S+")
 echo "Katana \\n.\\O \\l [$IP]" > /etc/issue
-
-# Configure SSH security settings
-sed -i "s/#PubkeyAuthentication yes/PubkeyAuthentication yes/" /etc/ssh/sshd_config
-sed -i "s/#PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/sshd_config
-echo "RequiredRSASize 2048" >> /etc/ssh/sshd_config
