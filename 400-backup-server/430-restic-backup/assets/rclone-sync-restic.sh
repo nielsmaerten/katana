642cf2b126e55250fcc0fb32378349472a439ebe
Niels Maerten <niels@maerten.be>
2025-06-28 08:51:32 +0200

    docs: update Restic backup profiles and rclone sync script with webhook URLs and scheduling adjustments

--- 400-backup-server/430-restic-backup/assets/rclone-sync-restic.sh
+++ 400-backup-server/430-restic-backup/assets/rclone-sync-restic.sh
@@ -8,10 +8,12 @@ SRC="/tank/restic"
 DEST="target-restic:"
 
 # Webhook to send after success
-SUCCESS_WEBHOOK="https://example.com/webhook/success"
+SUCCESS_WEBHOOK="{{ rclone_sync_restic_url_success }}"
+# Webhook to send after fail
+FAIL_WEBHOOK="{{ rclone_sync_restic_url_fail }}"
 
 # RCLONE command
-RCLONE_CMD="/usr/bin/rclone sync '$SRC' '$DEST' --log-file '$LOGFILE' --log-level DEBUG && curl '$SUCCESS_WEBHOOK' > /dev/null"
+RCLONE_CMD="/usr/bin/rclone sync '$SRC' '$DEST' --log-file '$LOGFILE' --log-level DEBUG && curl -d 'Restic Sync OK.' '$SUCCESS_WEBHOOK' > /dev/null || curl '$FAIL_WEBHOOK' > /dev/null"
 
 # Get lock and run rclone
 /usr/bin/flock /var/lock/rclone-sync.lock -c "$RCLONE_CMD"
